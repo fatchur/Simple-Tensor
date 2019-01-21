b5b74a2e8bdbded8970fe7d76b0c3e1d6642cbe6
fatchur <fatchur.rahman1@gmail.com>
2019-01-21 18:16:03 +0700

    update

--- simple_tensor/rnn_packs/pack.py
+++ simple_tensor/rnn_packs/pack.py
@@ -5,13 +5,15 @@ import numpy as np
 
 
 class LSTM(object):
-	def __init__(self, input_feature_num, output_feature_num, memory_feature_num, dropout_val=0.75, data_type=tf.float64):
+	def __init__(self, input_feature_num, output_feature_num, memory_feature_num, dropout_val=0.85, data_type=tf.float64):
 		"""
 		CLSTM Constructor
 		Args:
 			input_feature_num   :		an integer, the number of input feature
 			output_feature_num  :		an integer, the number of the output feture
 			memory_feature_num  :       an integer, the number of LSTM feature memory
+			dropout_val			:		a float
+			data_type			:		a tensorflow data type
 		"""
 		# the number of feature as input vector
 		self.input_feature_num = input_feature_num
@@ -50,21 +52,94 @@ class LSTM(object):
 		Return:
 			the output tensor and variable list of this block
 		"""
-		# first convolution layer
-		fc1, w_fc1, b_fc1 = new_fc_layer(self.inside_LSTM_nn_input, self.nn_inside_LSTM_inputfeature_num, layer_out_num1, \
-											name='fc1_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
-		#bn1, bn1_b, bn1_g = batch_norm(fc1, 1, name='bn1' + nn_code +"_" + cell_code, is_convolution=False)
+		# first fully connected layer + dropout
+		if cell_code != '0':
+			fc1, w_fc1, b_fc1 = new_fc_layer(self.inside_LSTM_nn_input, self.nn_inside_LSTM_inputfeature_num, layer_out_num1, \
+												name='fc1_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
+		else:
+			fc1, w_fc1, b_fc1 = new_fc_layer(self.inside_LSTM_nn_input, self.nn_inside_LSTM_inputfeature_num-1, layer_out_num1, \
+												name='fc1_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
 		drop1 = tf.nn.dropout(fc1, self.dropout_val)
-		# second convolution layer
+		# second fully connected layer + dropout
 		fc2, w_fc2, b_fc2 = new_fc_layer(drop1, layer_out_num1, layer_out_num2,\
 											name='fc2_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
-		#bn2, bn2_b, bn2_g = batch_norm(fc2, 1, name='bn2' + nn_code +"_" + cell_code, is_convolution=False)
 		drop2 = tf.nn.dropout(fc2, self.dropout_val)
-		# third convolution layer
+		# third fully connected layer + dropout
 		fc3, w_fc3, b_fc3 = new_fc_layer(drop2, layer_out_num2, layer_out_num3,\
 											 name='fc3_nn' + nn_code +"_" + cell_code, activation="none", data_type=self.tf_data_type)
 		drop3 = tf.nn.dropout(fc3, self.dropout_val)
-		vars = [w_fc1, b_fc1, w_fc2, b_fc2, w_fc3, b_fc3] #, bn1_b, bn1_g, bn2_b, bn2_g]
+		# variable list
+		vars = [w_fc1, b_fc1, w_fc2, b_fc2, w_fc3, b_fc3]
+		return drop3, vars
+
+
+	def inside_LSTM_hybridnn(self, layer_out_num1, layer_out_num2, layer_out_num3, nn_code, cell_code):
+		"""
+		A function of neural netwok block inside LSTM. 
+		Args:
+			layer_out_num1      :		an integer, the number of output from layer 1 int this block
+			layer_out_num2      :		an integer, the number of output from layer 2 int this block
+			layer_out_num3      :		an integer, the number of output from layer 3 int this block
+			nn_code             :       a string, the code for this block (just for graph naming)
+			cell_code           :       a string, the code for the LSTM cell (just for graph naming)
+		Return:
+			the output tensor and variable list of this block
+		"""
+		#######################################
+		###### fully connected block ##########
+		#######################################
+		# first fully connected layer + drop out
+		if cell_code != '0':
+			fc1, w_fc1, b_fc1 = new_fc_layer(self.inside_LSTM_nn_input, self.nn_inside_LSTM_inputfeature_num, layer_out_num1, \
+												name='fc1_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
+		else:
+			fc1, w_fc1, b_fc1 = new_fc_layer(self.inside_LSTM_nn_input, self.nn_inside_LSTM_inputfeature_num-1, layer_out_num1, \
+												name='fc1_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
+		drop1 = tf.nn.dropout(fc1, self.dropout_val)
+		# second fully connected layer + drop out
+		fc2, w_fc2, b_fc2 = new_fc_layer(drop1, layer_out_num1, layer_out_num2,\
+											name='fc2_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
+		drop2 = tf.nn.dropout(fc2, self.dropout_val)
+
+		#######################################
+		###### convolution block 1 ############
+		#######################################
+		# convolution layer 1
+		batch = tf.shape(self.inside_LSTM_nn_input)[0]
+		width = tf.shape(self.inside_LSTM_nn_input)[1]
+		conv1a, conv1a_w, conv1a_b = new_conv1d_layer(input=tf.reshape(self.inside_LSTM_nn_input, [batch, width, 1]), filter_shape=[3, 1, 32], name='conv1d1a' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		conv1b, conv1b_w, conv1b_b = new_conv1d_layer(input=tf.reshape(self.inside_LSTM_nn_input, [batch, width, 1]), filter_shape=[5, 1, 32], name='conv1d1b' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		conv1c, conv1c_w, conv1c_b = new_conv1d_layer(input=tf.reshape(self.inside_LSTM_nn_input, [batch, width, 1]), filter_shape=[7, 1, 32], name='conv1d1c' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		# convolution layer 2
+		conv2a, conv2a_w, conv2a_b = new_conv1d_layer(input=conv1a, filter_shape=[3, tf.shape(conv1a)[-1], 32], name='conv1d2a' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		conv2b, conv2b_w, conv2b_b = new_conv1d_layer(input=conv1b, filter_shape=[5, tf.shape(conv1b)[-1], 32], name='conv1d2b' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		conv2c, conv2c_w, conv2c_b = new_conv1d_layer(input=conv1c, filter_shape=[7, tf.shape(conv1c)[-1], 32], name='conv1d2c' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		# concatenate 
+		concat1 = tf.concat([conv2a, conv2b, conv2c], -1)
+
+		#######################################
+		###### convolution block 2 ############
+		#######################################
+		# convolution layer 1
+		conv3a, conv3a_w, conv3a_b = new_conv1d_layer(input=concat1, filter_shape=[3, tf.shape(concat1)[-1], 32], name='conv1d3a' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		conv3b, conv3b_w, conv3b_b = new_conv1d_layer(input=concat1, filter_shape=[5, tf.shape(concat1)[-1], 32], name='conv1d3b' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		# convolution layer 2
+		conv4a, conv4a_w, conv4a_b = new_conv1d_layer(input=conv3a, filter_shape=[5, tf.shape(conv3a)[-1], 128], name='conv1d4' + nn_code +"_" + cell_code, strides=2, data_type=self.tf_data_type)
+		conv4b, conv4b_w, conv4b_b = new_conv1d_layer(input=conv3b, filter_shape=[3, tf.shape(conv3b)[-1], 128], name='conv1d5' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
+		# concatenate 
+		concat2 = tf.concat([conv3a, conv3b], -1)
+		# flatten
+		flatten1 = tf.layers.flatten(concat2)
+
+		#########################################################
+		### combine fully connected result with convolution #####
+		#########################################################
+		concat3 = tf.concat([drop2, flatten1], 1)
+		out, w_out, b_out = new_fc_layer(concat3, tf.shape(concat3)[1], layer_out_num3, \
+											name='fc1_nn' + nn_code +"_" + cell_code, activation="LRELU", data_type=self.tf_data_type)
+		# variable list
+		vars = [w_fc1, b_fc1, w_fc2, b_fc2, conv1a_w, conv1a_b, conv1b_w, conv1b_b, conv1c_w, conv1c_b, conv2a_w, conv2a_b, conv2b_w, conv2b_b, conv2c_w, conv2c_b, 
+				conv3a_w, conv3a_b, conv3b_w, conv3b_b, w_out, conv4a_w, conv4a_b, conv4b_w, conv4b_b, w_out, b_out]
 		return drop3, vars
 
 
@@ -80,31 +155,38 @@ class LSTM(object):
 		Return:
 			the output tensor and variable list of this block
 		"""
-		# first convolution layer
+		#######################################
+		###### convolution block 1 ############
+		#######################################
+		# convolution layer 1
 		batch = tf.shape(self.inside_LSTM_nn_input)[0]
 		width = tf.shape(self.inside_LSTM_nn_input)[1]
 		conv1a, conv1a_w, conv1a_b = new_conv1d_layer(input=tf.reshape(self.inside_LSTM_nn_input, [batch, width, 1]), filter_shape=[3, 1, 32], name='conv1d1a' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 		conv1b, conv1b_w, conv1b_b = new_conv1d_layer(input=tf.reshape(self.inside_LSTM_nn_input, [batch, width, 1]), filter_shape=[5, 1, 32], name='conv1d1b' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 		conv1c, conv1c_w, conv1c_b = new_conv1d_layer(input=tf.reshape(self.inside_LSTM_nn_input, [batch, width, 1]), filter_shape=[7, 1, 32], name='conv1d1c' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
-
+		# convolution layer 2
 		conv2a, conv2a_w, conv2a_b = new_conv1d_layer(input=conv1a, filter_shape=[3, tf.shape(conv1a)[-1], 32], name='conv1d2a' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 		conv2b, conv2b_w, conv2b_b = new_conv1d_layer(input=conv1b, filter_shape=[5, tf.shape(conv1b)[-1], 32], name='conv1d2b' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 		conv2c, conv2c_w, conv2c_b = new_conv1d_layer(input=conv1c, filter_shape=[7, tf.shape(conv1c)[-1], 32], name='conv1d2c' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
-
+		# convolution layer 3
 		conv3a, conv3a_w, conv3a_b = new_conv1d_layer(input=conv2a, filter_shape=[3, tf.shape(conv2a)[-1], 64], name='conv1d3a' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 		conv3b, conv3b_w, conv3b_b = new_conv1d_layer(input=conv2b, filter_shape=[5, tf.shape(conv2b)[-1], 64], name='conv1d3b' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 		conv3c, conv3c_w, conv3c_b = new_conv1d_layer(input=conv2c, filter_shape=[7, tf.shape(conv2c)[-1], 64], name='conv1d3c' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
-
+		
+		#######################################
+		###### convolution block 2 ############
+		#######################################
+		# convolution layer 1
 		concat1 = tf.concat([conv3a, conv3b, conv3c], -1)
 		conv4, conv4_w, conv4_b = new_conv1d_layer(input=concat1, filter_shape=[5, tf.shape(concat1)[-1], 128], name='conv1d4' + nn_code +"_" + cell_code, strides=2, data_type=self.tf_data_type)
 		conv5, conv5_w, conv5_b = new_conv1d_layer(input=conv4, filter_shape=[3, tf.shape(conv4)[-1], 128], name='conv1d5' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
-
+		# convolution layer 2
 		conv6, conv6_w, conv6_b = new_conv1d_layer(input=conv5, filter_shape=[5, tf.shape(conv5)[-1], 128], name='conv1d6' + nn_code +"_" + cell_code, strides=2, data_type=self.tf_data_type)
 		conv7, conv7_w, conv7_b = new_conv1d_layer(input=conv6, filter_shape=[3, tf.shape(conv6)[-1], 128], name='conv1d7' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
-
+		# convolution layer 3
 		conv8, conv8_w, conv8_b = new_conv1d_layer(input=conv7, filter_shape=[5, tf.shape(conv7)[-1], 128], name='conv1d8' + nn_code +"_" + cell_code, strides=2, data_type=self.tf_data_type)
 		conv9, conv9_w, conv9_b = new_conv1d_layer(input=conv8, filter_shape=[3, tf.shape(conv8)[-1], 128], name='conv1d9' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
-
+		# convolution layer 4
 		conv10, conv10_w, conv10_b = new_conv1d_layer(input=conv9, filter_shape=[5, tf.shape(conv9)[-1], 128], name='conv1d10' + nn_code +"_" + cell_code, strides=2, data_type=self.tf_data_type)
 		conv11, conv11_w, conv11_b = new_conv1d_layer(input=conv10, filter_shape=[3, tf.shape(conv10)[-1], 128], name='conv1d11' + nn_code +"_" + cell_code, data_type=self.tf_data_type)
 
@@ -134,21 +216,24 @@ class LSTM(object):
 		Return:
 			the output tensor, feature memory and variable list of this LSTM cell
 		"""
-		s1 = tf.concat([input_tensor, last_output], axis=1, name='concat_' + str(cell_code))
+		if cell_code != '0':
+			s1 = tf.concat([input_tensor, last_output], axis=1, name='concat_' + str(cell_code))
+		else:
+			s1 = input_tensor
 		self.inside_LSTM_nn_input = s1
 
 		## forget block
 		s2, s2_vars = self.inside_LSTM_nn(num_hidden_neuron, num_hidden_neuron, self.nn1_inside_LSTM_outputfeature_num, '1', cell_code)
 		s2 = tf.nn.sigmoid(s2, name = 's2_sig_' + cell_code)
 
-		## nn learn block 1
+		## Learn block1 (sigmoid)
 		if deep_lstm:
 			s3, s3_vars = self.inside_LSTM_deepnn(num_hidden_neuron, num_hidden_neuron, self.nn2_inside_LSTM_outputfeature_num, '2', cell_code)
 		else:
 			s3, s3_vars = self.inside_LSTM_nn(num_hidden_neuron, num_hidden_neuron, self.nn2_inside_LSTM_outputfeature_num, '2', cell_code)
 		s3 = tf.nn.sigmoid(s3, name = 's3_sig_' + cell_code)
 
-		## nn learn block 2 (tanh)
+		## Learn block 2 (tanh)
 		#if deep_lstm:
 		#	s4, s4_vars = self.inside_LSTM_deepnn(num_hidden_neuron, num_hidden_neuron, self.nn3_inside_LSTM_outputfeature_num, '3', cell_code)
 		#else:
@@ -164,12 +249,14 @@ class LSTM(object):
 
 		s6 = tf.multiply(s3, s4, name = 'multiply_s3_s4_' + cell_code)
 
-		s7 = tf.multiply(last_memmory, s2, name = 'multiply_s2_memory_' + cell_code)
-
+		if cell_code != '0':
+			s7 = tf.multiply(last_memmory, s2, name = 'multiply_s2_memory_' + cell_code)
+		else:
+			s7 = s2
+			
+		# current memory
 		s8 = tf.add(s6, s7, name='add_s6_s7_' + cell_code)
-
 		s9 = tf.tanh(s8, name='tanh_s8_' + cell_code)
-
 		s10 = tf.multiply(s5, s9, name = 'multiply_s5_s10_' + cell_code)
 		drop_s10 = tf.nn.dropout(s10, self.dropout_val)
 
@@ -249,15 +336,19 @@ class SimpleSquenceLSTM(LSTM):
 		Return:
 			the output tensor and variable list of this LSTM cell
 		"""
+		last_output = None
+		last_memmory = None
 		outs = []
 		cell_vars = []
 		memmories = {}
 		for i in range(self.num_lstm_cell):
+			'''
 			if i == 0:
 				last_output = tf.convert_to_tensor(np.zeros((self.batch_size, self.output_feature_num)).astype(self.np_data_type))
 				last_memmory = tf.convert_to_tensor(np.zeros((self.batch_size, self.memory_feature_num)).astype(self.np_data_type))
+			'''
 
-			cell_input = tf.reshape(self.input_feature_placeholder[:, i, :], [self.batch_size, self.input_feature_num])
+			cell_input = self.input_feature_placeholder[:, i, :]
 			out, memory, cell_var = self.build_lstm_cell(last_output, last_memmory, cell_input, num_hidden_neuron = self.num_hidden_neuron, cell_code= scoope + str(i), deep_lstm=deep_lstm)
 			last_output = out
 			memmories[i] = memory
@@ -267,8 +358,9 @@ class SimpleSquenceLSTM(LSTM):
 				last_memmory = memory 
 			outs.append(out)
 			cell_vars.extend(cell_var)
-
-		outs = tf.reshape(outs, [self.batch_size, self.num_lstm_cell, self.output_feature_num])
+		print ('out shape', outs[0])
+		outs = tf.transpose(tf.stack(outs), [1, 0, 2])
+		print (outs)
 
 		if self.return_memmory:
 			memmories = tf.reshape(list(memmories.values()), [self.batch_size, self.num_lstm_cell, self.memory_feature_num])
